f9c44678b428dd75ffb0d7e6f6ff8d436988301f
Zakaria Bougroug <Zakaria.hammani.bougroug@gmail.com>
2019-03-08 02:41:28 +0100

    Add files via upload

---
+++ TodoListProject/assets/js/todos.js
@@ -0,0 +1,36 @@
+//Check off todos by clicking
+
+
+//If li is grey, turn it black
+
+
+
+$("ul").on("click", "li", function(){
+$(this).toggleClass("completed");
+});
+
+//Click on X to delete todo
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+event.stopPropagation();
+})
+
+
+$("input[type='text']").keypress(function(event){
+
+    if(event.which === 13){
+    console.log($(this).val());  
+    var todoText = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span><i class='fas fa-trash'></i></span> " + todoText + "</li>");
+ }
+   
+
+});
+
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+})
